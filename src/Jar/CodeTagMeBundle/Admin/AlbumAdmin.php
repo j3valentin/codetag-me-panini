149624822b469c88ef0e352d3704da2ed3d846a9
Johan AR <j3valentin@gmail.com>
2014-07-25 18:08:18 -0500

    Creando los cruds para las entidades

---
+++ src/Jar/CodeTagMeBundle/Admin/AlbumAdmin.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Jar\CodeTagMeBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+/**
+ * Description of AlbumAdmin
+ *
+ * @author Johan AR <j3valentin@gmail.com>
+ */
+class AlbumAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name')
+            ->add('description')
+            ->add('year')
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('personJuridic')
+            ->add('name')
+            ->add('type')
+            ->add('floors')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('description')
+            ->add('year')
+        ;
+    }
+
+}
